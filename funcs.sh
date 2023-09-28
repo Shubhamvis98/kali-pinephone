b81dab841e145e59636c17e6137972305ac97bad
ShubhamVis98 <43336210+Shubhamvis98@users.noreply.github.com>
2023-09-28 11:21:06 +0530

    systemd-nspawn env fix

--- funcs.sh
+++ funcs.sh
@@ -3,7 +3,7 @@
 ARCH='arm64'
 qemu_bin='/usr/bin/qemu-aarch64-static'
 machine='debian'
-ENV="RUNLEVEL=1,LANG=C,DEBIAN_FRONTEND=noninteractive,DEBCONF_NOWARNINGS=yes"
+ENV='-E RUNLEVEL=1 -E LANG=C -E DEBIAN_FRONTEND=noninteractive -E DEBCONF_NOWARNINGS=yes'
 LOOP=`losetup -f`
 BOOT_P=${LOOP}p1
 ROOT_P=${LOOP}p2
